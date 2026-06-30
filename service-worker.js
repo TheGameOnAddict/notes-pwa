82e253fc452f867004cebf87cd3613966d64c5e2
TheGameOnAddict <96160502+TheGameOnAddict@users.noreply.github.com>
2026-06-30 00:31:04 -0400

    Updated ui!

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='pdf-markup-ios26-reddit-fix-v1';
+const CACHE_NAME='pdf-markup-clean-compact-ui-v1';
 const APP_FILES=['./','index.html','manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
