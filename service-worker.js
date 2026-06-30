feebab52713d23a775b785e49a5ebf8a7cdf599e
TheGameOnAddict <96160502+TheGameOnAddict@users.noreply.github.com>
2026-06-29 23:42:32 -0400

    Ui

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='pdf-markup-ios26-refined-v1';
+const CACHE_NAME='pdf-markup-ios26-safearea-v1';
 const APP_FILES=['./','index.html','manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
