1ecab394c48cc70c19548b474a32b9550fcc150d
TheGameOnAddict <96160502+TheGameOnAddict@users.noreply.github.com>
2026-06-30 02:16:58 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ipseer-viewport-copy-v31-v1';
+const CACHE_NAME='ipseer-v32-refined-viewport-copy-v1';
 const APP_FILES=['./','index.html','manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
